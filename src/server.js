f216bcb39abb331362e2281e4a1114364451372a
lpolon <lpolon@gmail.com>
2020-04-12 14:27:15 -0300

    added mongoose setup

--- src/server.js
+++ src/server.js
@@ -8,6 +8,23 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import errorHandler from 'errorhandler'
 
+import mongoose from 'mongoose'
+
+// setup to deal with DeprecationWarning:
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(res => {
+    console.log(`Connected to Mongo! Database name: "${res.connection.name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  })
+
 app.use(bodyParser.json())
 app.use(cors())
 app.use(morgan('dev'))
@@ -16,6 +33,7 @@ app.use(morgan('dev'))
 
 const PORT = process.env.PORT || 4000
 
+// TODO: Lembrar: express pega erros síncronos, erros assíncronos precisam ser tratados chamando next(err) no catch()
 app.use(errorHandler())
 
 app.listen(PORT, () => {
