c20e701fab1e03a5c0fcc42f7f14542890530092
lpolon <lpolon@gmail.com>
2020-04-14 20:18:56 -0300

    moved isPasswordAllowed function back to util/auth.js
    realized that it was not going to be used as validade since the document will only save the hashedPassword

--- models/User.js
+++ models/User.js
@@ -68,18 +68,4 @@ function isUsernameAllowed(usernameValue) {
   )
 }
 
-function isPasswordAllowed(password) {
-  return (
-    password.length > 6 &&
-    // non-alphanumeric
-    /\W/.test(password) &&
-    // digit
-    /\d/.test(password) &&
-    // capital letter
-    /[A-Z]/.test(password) &&
-    // lowercase letter
-    /[a-z]/.test(password)
-  )
-}
-
 export const User = model('User', userSchema)
