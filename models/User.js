8cb11506b5cd590a9afdeaaa13b4809c2a8c3d77
lpolon <lpolon@gmail.com>
2020-04-13 22:04:50 -0300

    Usermodel has custom validation to unique username

--- models/User.js
+++ models/User.js
@@ -5,6 +5,8 @@ const userSchema = new Schema(
     username: {
       type: String,
       required: true,
+      // validate pode ser uma função, um array para custom err msg ou array de objetos: [{validator: validator, msg: 'lala'}]
+      validate: [isUsernameUnique, 'Username already taken'],
     },
     password: {
       type: String,
@@ -21,7 +23,29 @@ const userSchema = new Schema(
   },
 )
 /*
- TODO: De que é a responsabilidade de cryptgrafar o pw?
+ TODO: De quem é a responsabilidade de cryptgrafar o pw?
  O delete: buscar e deletar groupos do mesmo usuário
 */
+
+// userSchema.path('username').validate(function(value, done) {
+
+// })
+
+/*
+https://mongoosejs.com/docs/api.html#schematype_SchemaType-validate
+é uma função async, então retorna uma Promise. on resolve, true ou undefined === validation passed, false ou falsy === validation failed. É um .pre('save') hook middleware que roda on .save() ou .validade(). Se a função fosse síncrona, validar com validateSync().
+
+No mais: Se eu precisar de novo de validação única, por favor, abstraia melhor.
+*/
+async function isUsernameUnique(usernameValue, done) {
+  try {
+    const queryResult = await this.model('User').find({username: usernameValue})
+    return queryResult.length === 0 ? true : false
+  } catch (error) {
+    throw Error(
+      'Olá. isUserNameUnique foi rejeitada e isso jamais deveria acontecer. Boa sorte. Beijos de luz',
+    )
+  }
+}
+
 export const User = model('User', userSchema)
