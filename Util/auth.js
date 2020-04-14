4cbe3082f2bd107db64e8524c048f8205e854b86
lpolon <lpolon@gmail.com>
2020-04-14 18:34:35 -0300

    moved isPasswordAllowed() to model/User.js to keep it consistent com validators defined in file

--- Util/auth.js
+++ Util/auth.js
@@ -16,17 +16,3 @@ export async function hashPassword(user) {
   })
   return hashPassword
 }
-
-export function isPasswordAllowed(password) {
-  return (
-    password.length > 6 &&
-    // non-alphanumeric
-    /\W/.test(password) &&
-    // digit
-    /\d/.test(password) &&
-    // capital letter
-    /[A-Z]/.test(password) &&
-    // lowercase letter
-    /[a-z]/.test(password)
-  )
-}
--- models/User.js
+++ models/User.js
@@ -48,4 +48,19 @@ async function isUsernameUnique(usernameValue, done) {
   }
 }
 
+
+function isPasswordAllowed(password) {
+  return (
+    password.length > 6 &&
+    // non-alphanumeric
+    /\W/.test(password) &&
+    // digit
+    /\d/.test(password) &&
+    // capital letter
+    /[A-Z]/.test(password) &&
+    // lowercase letter
+    /[a-z]/.test(password)
+  )
+}
+
 export const User = model('User', userSchema)
