7ddd5cabf027a8c67f1f9608d2ba231de45d2cdd
lpolon <lpolon@gmail.com>
2020-04-15 16:52:56 -0300

    removed useless hashpassword function bcrypt.hash already returns a promise

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,7 +1,9 @@
 import {Router} from 'express'
 import {User} from '../models/User'
 
-import {isPasswordAllowed, hashPassword} from '../../util/auth'
+import {isPasswordAllowed} from '../../util/auth'
+
+import bcrypt from 'bcrypt'
 
 const router = Router()
 
@@ -13,24 +15,19 @@ router.post('/', async (req, res, next) => {
 
   if (!isPasswordAllowed(password))
     return res.status(400).json({message: `password is not strong enough`})
-
   try {
-    const hashedPassword = await hashPassword(password)
-  } catch (error) {
-    next(error)
-  }
-  const newUser = new User({
-    username,
-    password: hashPassword,
-  })
-  try {
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    })
     const savedUser = await newUser.save()
     // response should send token
     res
       .status(201)
       .json({message: `new user ${savedUser.username} saved sucessfully`})
   } catch (error) {
-    // Não realmente consegui pensar num caso em que caia nesse catch e o erro não contenha o erro de validação.
     const usernameObj = error?.errors?.username
     if (typeof usernameObj === 'undefined') return res.sendStatus(500)
     const {message} = usernameObj
--- util/auth.js
+++ util/auth.js
@@ -1,20 +1,3 @@
-/*
-bcrypt.hash() já retorna uma promise A única vantagem de separar essa função é "fixar" o saltRounds. É um uso adequado de promise de qualquer forma.
-*/
-
-import bcrypt from 'bcrypt'
-
-export function hashPassword(password) {
-  const saltRounds = 10
-  // TODO: Dúvida: Eu deveria retornar o salt?
-  return new Promise((resolve, reject) => {
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-      if (err) return reject(err)
-      resolve(hash)
-    })
-  })
-}
-
 export function isPasswordAllowed(password) {
   return (
     password.length > 6 &&
