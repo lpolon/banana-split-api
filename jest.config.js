b60c1628a4a9e3b1be107208f9f812039430fe12
lpolon <lpolon@gmail.com>
2020-04-15 19:59:13 -0300

    added config in jest.config.js to ignore ./dist folder tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
+const path = require('path')
+
 module.exports = {
   testEnvironment: 'node',
   // anything that is in the src directory
-  collectCoverageFrom: ['**/src/**/*.js']
+  collectCoverageFrom: ['**/src/**/*.js'],
+  // testMatch: ['**/src/**/*.js'],
+  rootDir: path.join(__dirname, '.'),
+  testPathIgnorePatterns: ['<rootDir>/dist/'],
 }
