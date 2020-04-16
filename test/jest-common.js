67d6df6d8b79db49726a1e725f575604e8ccac68
lpolon <lpolon@gmail.com>
2020-04-15 23:51:19 -0300

    added jest-watch plugins to filter tests in watch mode

--- test/jest-common.js
+++ test/jest-common.js
@@ -4,4 +4,5 @@ module.exports = {
   testEnvironment: 'node',
   rootDir: path.join(__dirname, '..'),
   testPathIgnorePatterns: ['<rootDir>/dist/'],
+  watchPlugins: ['jest-watch-select-projects', 'jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
 }
