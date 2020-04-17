55989a724f988f73fd78227656242b50d96354ca
lpolon <lpolon@gmail.com>
2020-04-17 05:17:13 -0300

    added authenticateToken middleware in src/utils/auth.js - also added GET api/ as a protected route (a route that requires authorization) for monkey-testing

--- src/util/auth.js
+++ src/util/auth.js
@@ -1,3 +1,5 @@
+import { verify } from 'jsonwebtoken';
+
 export function isPasswordAllowed(password) {
   return (
     password.length > 6 &&
@@ -10,4 +12,21 @@ export function isPasswordAllowed(password) {
     // lowercase letter
     /[a-z]/.test(password)
   );
+}
+
+// authentication middleware:
+export function authenticateToken(req, res, next) {
+  // get the token from header
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1]; // TOKEN FROM BEARER TOKEN
+  // is there a token?
+  if (typeof token === 'undefined') return res.sendStatus(401);
+  // is it valid?
+  // verify does not return a promise
+  verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(user);
+    if (err) return res.sendStatus(403); // 403 => you have a token, but it is no longer valid
+    req.user = user;
+    next();
+  });
 }
