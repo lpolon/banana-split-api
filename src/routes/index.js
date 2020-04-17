943808d9248a4b5cc362e44388aaec23193f81a3
lpolon <lpolon@gmail.com>
2020-04-17 05:17:13 -0300

    added authenticateToken middleware in src/utils/auth.js - also added GET api/ as a protected route (a route that requires authorization) for monkey-testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import registerRouter from './register';
+import loginRouter from './login';
+import { authenticateToken } from '../util/auth';
+
 const router = Router();
 router.use('/register', registerRouter);
+router.use('/login', loginRouter);
 
-router.get('/', (req, res, next) => {
+// monkey-testing auth middleware
+router.get('/', authenticateToken, (req, res, next) => {
   res.json({ message: 'hello, world' });
 });
 
