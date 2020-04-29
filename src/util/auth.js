6f732c9509a2dbad91d2a905b3e88e5efde99312
lpolon <lpolon@gmail.com>
2020-04-28 23:43:27 -0300

    refactor of authenticateTokenMiddleware to use express-JWT package

--- src/util/auth.js
+++ src/util/auth.js
@@ -2,7 +2,10 @@ import { verify, sign } from 'jsonwebtoken';
 import { compare } from 'bcrypt';
 
 import LocalStrategy from 'passport-local';
+import expressJWT from 'express-jwt';
 import { User } from '../models/User';
+
+const secret = String(process.env.ACCESS_TOKEN_SECRET);
 export function isPasswordAllowed(password) {
   return (
     password.length > 6 &&
@@ -51,28 +54,31 @@ export function getUserToken({ _id, username }) {
     iat: issuedAt,
     exp: issuedAt + sixtyDaysInSeconds,
   };
-  return sign(claims, process.env.ACCESS_TOKEN_SECRET);
+  return sign(claims, secret);
 }
 
 // authentication middleware:
-// TODO: substitute by passport-JWT or express-jwt
-export function authenticateTokenMiddleware(req, res, next) {
-  // get the token from header
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')[1]; // TOKEN FROM BEARER TOKEN
-  // is there a token?
-  if (typeof token === 'undefined') return res.sendStatus(401);
-  // is it valid?
-  // verify does not return a promise
-  verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) {
-      next(err);
-      return res.sendStatus(403);
-    } // 403 => you have a token, but it is no longer valid
-    req.user = user;
-    next();
-  });
-}
+// export function authenticateTokenMiddleware(req, res, next) {
+//   // get the token from header
+//   const authHeader = req.headers.authorization;
+//   const token = authHeader && authHeader.split(' ')[1]; // TOKEN FROM BEARER TOKEN
+//   // is there a token?
+//   if (typeof token === 'undefined') return res.sendStatus(401);
+//   // is it valid?
+//   // verify does not return a promise
+//   verify(token, secret, (err, user) => {
+//     if (err) {
+//       next(err);
+//       return res.sendStatus(403);
+//     } // 403 => you have a token, but it is no longer valid
+//     req.user = user;
+//     next();
+//   });
+// }
+
+export const authenticateTokenMiddleware = expressJWT({
+  secret,
+});
 
 /*
 TODO: Write tests to when username is undefined, when password is wrong, when username and password is ok to make sure it is throwing correctly
