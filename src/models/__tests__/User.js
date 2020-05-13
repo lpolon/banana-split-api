cf5152bab2db632307fcb4cee1310b61fa3eee8b
lpolon <lpolon@gmail.com>
2020-05-13 00:18:06 -0300

    changes in User model tests - added password to user document to test invalid username errors - added Validation error tests to password - refactored some username and password to use generate util to better communicate the intention of the test

--- src/models/__tests__/User.js
+++ src/models/__tests__/User.js
@@ -4,52 +4,25 @@ import { resolve } from '../../../test/util/async';
 import * as generate from '../../../test/util/generate';
 
 describe('the username path:', () => {
-  it('is a string', () => {
-    const username = generate.username();
-    const user = new User({
-      username,
-    });
-    expect(user.username).toStrictEqual(username);
-  });
   it('is required', () => {
     const emptyUsername = '';
+    const password = generate.password();
     const user = new User({
       username: emptyUsername,
+      password,
     });
     const error = user.validateSync();
     expect(error).toMatchInlineSnapshot(
       `[ValidationError: User validation failed: username: Path \`username\` is required.]`,
     );
   });
-  it('invalidates usernames with less than 3 characters', () => {
-    const shortUsername = '1_';
-    const user = new User({
-      username: shortUsername,
-    });
-    const error = user.validateSync();
-    expect(error).toMatchInlineSnapshot(
-      `[ValidationError: User validation failed: username: invalid username]`,
-    );
-  });
-
-  it('invalidates usernames with white spaces', () => {
-    const usernameWithWhiteSpace = 'leo polon';
-    const user = new User({
-      username: usernameWithWhiteSpace,
-    });
-
-    const error = user.validateSync();
-    expect(error).toMatchInlineSnapshot(
-      `[ValidationError: User validation failed: username: invalid username]`,
-    );
-  });
-
-  it('invalidates usernames with non-alphanumerical values', () => {
-    const invalidUsername = '@leo';
+  it('throws ValidationError for invalid usernames', () => {
+    const invalidUsername = '1_';
+    const password = generate.password();
     const user = new User({
       username: invalidUsername,
+      password,
     });
-
     const error = user.validateSync();
     expect(error).toMatchInlineSnapshot(
       `[ValidationError: User validation failed: username: invalid username]`,
@@ -57,6 +30,36 @@ describe('the username path:', () => {
   });
 });
 
+describe('the password path', () => {
+  it('is required', () => {
+    const username = generate.username();
+    const emptyPassword = '';
+    const user = new User({
+      username,
+      password: emptyPassword,
+    });
+    const error = user.validateSync();
+    expect(error).toMatchInlineSnapshot(
+      `[ValidationError: User validation failed: password: Path \`password\` is required.]`,
+    );
+  });
+  it('throws validation error for invalid password', () => {
+    const username = generate.username();
+    const invalidPassword = 'password';
+    const user = new User({
+      username,
+      password: invalidPassword,
+    });
+
+    const error = user.validateSync();
+    expect(error).toMatchInlineSnapshot(
+      `[ValidationError: User validation failed: password: password is not strong enough]`,
+    );
+  });
+});
+
+// TODO: How kent does this kind of test in his course?
+// TODO: test groups when group is deleted or created
 describe('the User model:', () => {
   const MONGODB_URI = 'mongodb://localhost/USER_TEST_2';
   const options = {
@@ -69,8 +72,10 @@ describe('the User model:', () => {
 
   it('creates a new user', async done => {
     const username = generate.username();
+    const password = generate.password();
     const user = new User({
       username,
+      password,
     });
 
     const savedUser = await user.save();
@@ -79,15 +84,18 @@ describe('the User model:', () => {
     expect(queryResult._id).toStrictEqual(savedUser._id);
     done();
   });
-
   it('throws validation error when #username already exists', async done => {
     const username = generate.username();
+    const password = generate.password();
     const user = new User({
       username,
+      password,
     });
 
+    const password2 = generate.password();
     const sameUser = new User({
       username,
+      password: password2,
     });
 
     await expect(user.validate()).resolves.toBeUndefined();
