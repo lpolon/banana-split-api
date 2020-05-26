8e702841014d4bf96ebcef0ddd63bde67c9cce70
lpolon <lpolon@gmail.com>
2020-05-25 21:31:13 -0300

    many changes in generate.js added factory functions to mock out the database and test controllers/routes

--- test/util/generate.js
+++ test/util/generate.js
@@ -9,27 +9,73 @@ const getUsername = faker.internet.userName;
 // prefex all of the ones we generate with `!0_Oo` to ensure it's valid.
 const getPassword = (...args) => `!0_Oo${faker.internet.password(...args)}`;
 const get_id = faker.random.uuid;
+const getTimestamps = () => ({
+  createdAt: faker.date.past(1),
+  updatedAt: faker.date.between(faker.date.past(1), new Date()),
+});
 
-// update it if the user changes
-// TODO: pretty sure that in this project, my buildUser() should return an instance of my User model
-// const user = new User({
-//   username: getUsername(),
-//   password: await getHashedPassword(password),
-//   groups: [],
-//   ...overrides,
-// });
-// return user;
+const randomPositiveInt = max => Math.floor(Math.random() * max) + 1;
+
+function buildMany(factoryFn) {
+  return Array.from({ length: randomPositiveInt(4) }, factoryFn);
+}
+
+function buildGroup(overrides) {
+  return {
+    _id: get_id(),
+    owner: get_id(),
+    expenses: buildMany(buildExpense),
+    settles: buildMany(buildSettle),
+    groupName: faker.lorem.words(2),
+    members: buildMany(getUsername),
+    description: faker.lorem.sentence(),
+    date: faker.date.recent(20),
+    ...getTimestamps(),
+    ...overrides,
+  };
+}
+
 async function buildUser({ password = getPassword(), ...overrides } = {}) {
   return {
     _id: get_id(),
     username: getUsername(),
     password: await getHashedPassword(password),
-    groups: [],
+    groups: buildMany(buildGroup),
+    ...getTimestamps(),
     ...overrides,
   };
 }
 
-// not yet in use. Just brought from kent's project
+// TODO: groups, settles and expenses estão representando mal o próprio schema, principalmente em relação à relação de Owner e Id. Isso provavelmente vai precisar ser alterado durante os testes.
+function buildSettle(overrides) {
+  return {
+    _id: get_id(),
+    owner: get_id(),
+    value: faker.random.number(),
+    paidBy: getUsername(),
+    paidTo: getUsername(),
+    ...getTimestamps(),
+    ...overrides,
+  };
+}
+
+function buildExpense(overrides) {
+  return {
+    _id: get_id(),
+    owner: get_id(),
+    description: faker.lorem.sentence(),
+    value: faker.random.number(),
+    split: {
+      paidBy: getUsername(),
+      dividedBy: buildMany(getUsername),
+      isDividedByAll: true,
+    },
+    ...getTimestamps(),
+    ...overrides,
+  };
+}
+
+// not in use yet. Just brought from kent's project
 function token(user) {
   return getUserToken(buildUser(user));
 }
@@ -65,6 +111,9 @@ export {
   buildRes,
   buildNext,
   buildUser,
+  buildGroup,
+  buildSettle,
+  buildExpense,
   getUsername as username,
   getPassword as password,
   get_id as _id,
