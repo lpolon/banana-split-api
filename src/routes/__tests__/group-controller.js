bd935c83942ae2c6d253b21a8bb9583b23ce40d3
lpolon <lpolon@gmail.com>
2020-05-25 21:23:25 -0300

    group controller tests setup

---
+++ src/routes/__tests__/group-controller.js
@@ -0,0 +1,15 @@
+import {
+  buildReq,
+  buildRes,
+  buildNext,
+  buildUser,
+} from '../../../test/util/generate';
+import * as groupController from '../group-controller';
+import { Group } from '../../models/Group';
+
+jest.mock('../../models/Group.js');
+
+beforeEach(jest.clearAllMocks);
+
+// TODO: models and generate Utils VOLTE AQUI
+test.todo('more generate Util to test properly');
